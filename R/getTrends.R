1a88f41bdbca141f951f9c9d44ed69dd757631fe
Lorenzo Rossi <lorereds@gmail.com>
2014-11-21 16:20:27 +0000

    R package to download Google Trends data

---
+++ R/getTrends.R
@@ -0,0 +1,62 @@
+############################################################## 
+#' Get Trend data from Google
+#'
+#' This function scrapes Google Trends and returns the results nicely formatted in a R dataframe
+#' 
+#' @param query The keyword to be searched. It will be auto escaped.
+#' 
+#' @param country Country name, i.e: United Kingdom, Italy, Germany, etc. Case INSENSITIVE. Deafult option is the worldwide research.
+#' 
+#' @param region Region name, i.e: England, Scotland, etc. Case INSENSITIVE. Deafult option is none.
+#' 
+#' @param date The time span. Default is all. Other options are: 'last 7 days', 'last 30 days', 'last 90 days', 'last year' or the year number, i.e '2014'
+#' 
+#' @export
+
+getTrends = function(query, country = "all", region = "none", date = "all"){
+  #Libraries
+  library(httr)
+  library(RCurl)
+  library(stringr)
+  library(dplyr)
+  load("data/Google_Trends_Data.RData")
+  
+  #Make all lower case to reduce error rate
+  country = tolower(country)
+  region = tolower(region)
+  date = tolower(date)
+  
+  #Check that country and region are correct and encode
+  if(country != "all"){
+    right_country = filter(GT_Nations, tolower(name) == country)
+    if(nrow(right_country) == 0)
+      stop("Error: The selected country is not available")
+    if(region != "none"){
+      right_country = filter(right_country, tolower(sub_name) == region)
+      if(nrow(right_country) == 0)
+        stop("Error: The selected region is not available")
+      geo = paste(unique(right_country$id), unique(right_country$sub_id), sep = "-")
+    }else{
+      geo = unique(right_country$id)
+    }
+  }else{
+    geo = "all"
+  }
+  
+  #Check that date is correct and encode
+  right_date = filter(GT_Dates, name == date)
+  if(nrow(right_date) == 0)
+    stop("Error: The selected timeframe is not available")
+  else
+    date = right_date$id
+  
+  #Get raw data
+  html_results = get_googletrend_raw_data(query, geo, date)
+  
+  #Make it a R dataframe
+  results = raw_to_df(html_results)
+  
+  #Return output
+  return(results)
+}
+
---
+++ R/get_googletrend_raw_data.R
@@ -0,0 +1,22 @@
+get_googletrend_raw_data = function(query, geo, date){
+  #Session
+  base_url = "http://www.google.co.uk/trends/fetchComponent?hl=en-US&tz&cmpt=q&content=1&cid=TIMESERIES_GRAPH_0&export=3"
+  
+  #Create get request
+    #Query  
+    my_url = paste0(base_url, "&q=", curlEscape(query))
+    #Date
+    if(date != "all")
+      my_url = paste0(my_url, "&date=", curlEscape(date))
+    #Geo
+    if(geo != "all")
+      my_url = paste0(my_url, "&geo=", curlEscape(geo))
+  
+  #Go to results page
+  message(paste0("Downloading data from: ",my_url))
+  results = GET(my_url, add_headers("Cookie" = "PREF=Fake_Cookie"))
+  html_results = content(results, "text")
+  
+  #Return raw results
+  return(html_results)
+}
